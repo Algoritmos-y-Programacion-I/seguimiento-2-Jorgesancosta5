608a88551794b2df5a3537690301e95e32bc455b
Jorge Andres Sanchez Acosta <1104547294@u.icesi.edu.co>
2025-09-26 12:51:02 -0500

    Avance

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,22 +1,65 @@
 package model;
 
 public class Cliente {
-    
+    private String nombre;
+    private int edad;
+    private String numero_cedula;
+    private Cuenta[] cuentas;
+    private int numeroCuentas;
+   
 
 
-    public Cliente(){
-
+    public Cliente(String nombre, int Edad, String numero_cedula){
+       this.nombre = nombre;
+       this.edad = edad;
+       this.numero_cedula = numero_cedula;
+       this.cuentas = new Cuenta[10];
+       this.numeroCuentas = 0;
 
     }
 
-     /*
-     * ATENCION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-     * El siguiente metodo esta incompleto.
-     * Agregue los parametros y retorno que sean pertinentes.
-     * Agregue la logica necesaria.
-     */
-    public void getCuentasList() {
+    public String getNombre() {
+        return nombre;
 
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+
+    }
+
+    public String getNumero_cedula() {
+        return numero_cedula;
+    }
+
+    public void setNumero_cedula(String numero_cedula) {
+        this.numero_cedula = numero_cedula;
+
+    }
+
+     public Cuentabancaria[] getCuentabancaria(){
+        return cuentas;
+    }
+
+     public void setCuentabancaria(Cuentabancaria[] cuentas ) {
+        this.cuentas = cuentas;
+
+    }
+
+     public int getNumeroCuentas() {
+        return numeroCuentas;
+    }
+
+    public void setNumeroCuentas(int numeroCuentas) {
+        this.numeroCuentas = numeroCuentas;
+    }
 }
--- src/model/Cuenta.java
+++ src/model/Cuenta.java
@@ -1,5 +1,40 @@
 package model;
 
 public class Cuenta {
-    
+    private String nombreBanco;
+    private TipoCuenta tipo;
+    private double saldo;
+   
+public void Cuenta(String nombreBanco, TipoCuenta tipo, double saldo, String numeroCuenta) {
+        this.nombreBanco = nombreBanco;
+        this.tipo = tipo;
+        this.saldo = saldo;
+        
+    }
+
+    public String getNombreBanco() {
+        return nombreBanco;
+    }
+
+    public void setNombreBanco(String nombreBanco) {
+        this.nombreBanco = nombreBanco;
+    }
+
+    public TipoCuenta getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoCuenta tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+   
 }
